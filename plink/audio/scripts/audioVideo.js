00dfff2ac73a323a6bbca63ed41d0dfbe59b3935
allisluv <allisonlove@Allisons-MacBook-Pro.local>
2017-05-04 18:49:58 -0700

    link to p5?

--- plink/audio/scripts/audioVideo.js
+++ plink/audio/scripts/audioVideo.js
@@ -1,64 +1,20 @@
-// p5 file
-// popping bubble wrap
+console.log("link works");
 
-function setup() {
-  var myCanvas = createCanvas(800, 250);
-  myCanvas.parent('mySketch');
-  background(211, 235, 238);
+var siz;
+var h;
+var s;
+var b;
+
+    //for the sketch page
+    var yPos = 0;
+function setup() {  // setup() runs once
+  frameRate(30);
 }
-
-
-function popping (x, y) {
-  if ((x <= (mouseX - 20) || x >= (mouseX + 20)) || (y <= (mouseY - 20) || y >= (mouseY + 20))) {
-    stroke(48, 84, 90);
-    fill(211, 235, 238);
-    ellipse(x, y, 30, 30);
-    stroke(238, 249, 250);
-    fill(238, 249, 250);
-    ellipse(x + 6, y - 7, 10, 5);
-  } else {
-    noStroke();
-    fill(238, 249, 250);
-    ellipse(x, y, 40, 40);
-    noFill();
-    stroke(48, 84, 90);
-    arc(x + 40, y, 50, 50, radians(135), radians(225));
-    arc(x - 40, y, 50, 50, radians(-45), radians(45));
-    arc(x, y + 40, 50, 50, radians(225), radians(315));
-    arc(x, y - 40, 50, 50, radians(45), radians(135));
+function draw() {  // draw() loops forever, until stopped
+  background(204);
+  yPos = yPos - 1;
+  if (yPos < 0) {
+    yPos = height;
   }
-}
-
-function bubbles (x, y) {
-      stroke(48, 84, 90);
-      fill(211, 235, 238);
-      ellipse(x, y, 30, 30);
-      stroke(238, 249, 250);
-      fill(238, 249, 250);
-      ellipse(x + 6, y - 7, 10, 5);
-}
-
-
-
-function draw () {
-
-  stroke(48, 84, 90);
-  fill(211, 235, 238);
-  rect(0, 0, 798, 248);
-
-  var x = 25;
-  var y = 25;
-
-  while (y <= 230) {
-    while (x <= 780) {
-      if (mouseIsPressed) {
-        popping (x, y);
-      } else {
-        bubbles (x, y);
-      }
-      x += 50;
-    }
-    y += 50;
-    x = 25;
-  }
+  line(0, yPos, width, yPos);
 }
